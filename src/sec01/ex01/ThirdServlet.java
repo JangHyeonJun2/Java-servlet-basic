dde0dc07c49e728d9845c9f4e6f2bde2e30aac3a
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-06-25 20:54:06 +0900

    서블릿 이해하기1

---
+++ src/sec01/ex01/ThirdServlet.java
@@ -0,0 +1,26 @@
+package sec01.ex01;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("ThirdServlet doGet method");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("ThirdServlet destroy method");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("ThirdServlet init method ");
+    }
+}
