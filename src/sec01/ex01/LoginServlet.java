4f0aad78cdbcc9ccfe8765ddb7ae71a10b82c90a
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-07-02 23:48:17 +0900

    로그인 서블릿 주석 추가

--- src/sec01/ex01/LoginServlet.java
+++ src/sec01/ex01/LoginServlet.java
@@ -6,7 +6,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+/*
+ * 로그인 서블릿
+ */
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
