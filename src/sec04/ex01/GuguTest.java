9634e85866a39cda8ddd39bc074f397a1a024842
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-07-11 18:28:50 +0900

    GuguTest.java 주석 추가

--- src/sec04/ex01/GuguTest.java
+++ src/sec04/ex01/GuguTest.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
+/*
+ * 구구단 테스트 예제!
+ */
 @WebServlet("/guguTest")
 public class GuguTest extends HttpServlet {
     @Override
