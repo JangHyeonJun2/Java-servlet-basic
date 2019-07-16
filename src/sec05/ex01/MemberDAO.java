7c6ea8c5952cf256ce0350dfdf4b3f0c40428461
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-07-16 23:10:27 +0900

    데이터베이스 로직 처리 부분 완료

--- src/sec05/ex01/MemberDAO.java
+++ src/sec05/ex01/MemberDAO.java
@@ -1,4 +1,82 @@
 package sec05.ex01;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberDAO {
+    public List listMembers(){
+        List list = new ArrayList();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try{
+            conn = connDB(); //데이터베이스 연결 메서드
+            pstmt = conn.prepareStatement("select * from t_member");
+            rs = pstmt.executeQuery();
+
+            while (rs.next()){
+                String id = rs.getString("id");
+                String pwd = rs.getString("pwd");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date joinDate = rs.getDate("joinDate");
+
+                MemberVO vo = new MemberVO();
+                vo.setId(id);
+                vo.setPwd(pwd);
+                vo.setName(name);
+                vo.setEmail(email);
+                vo.setJoinDate(joinDate);
+                list.add(vo);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
+    private Connection connDB(){
+        Connection conn = null;
+
+        String server = "localhost:3306"; // MySQL 서버 주소
+        String database = "study_db1"; // MySQL DATABASE 이름
+        String user_name = "root"; //  MySQL 서버 아이디
+        String password = "0653"; // MySQL 서버 비밀번호
+        //1.드라이버 로
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("mysql 드라이버 로딩 성공");
+        }catch (ClassNotFoundException e) {
+            System.err.println(" !! <JDBC 오류> Driver load 오류: " + e.getMessage());
+            e.printStackTrace();
+        }
+        //연결
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + "?allowPublicKeyRetrieval=true&useSSL=false&amp;autoReconnection=true", user_name, password);
+        } catch (SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+        return conn;
+    }
 }
--- src/sec05/ex01/MemberServlet.java
+++ src/sec05/ex01/MemberServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import java.util.List;
 
 @WebServlet("/member")
@@ -16,22 +17,22 @@ public class MemberServlet extends HttpServlet {
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
         MemberDAO dao = new MemberDAO(); //SQL문으로 조회할 MemberDAO객체를 생성합니다.
-        //List list = dao.listMembers(); //listMembers() 메서드로 회원 정보를 조회합니다.
+        List list = dao.listMembers(); //listMembers() 메서드로 회원 정보를 조회합니다.
 
         out.print("<html><body>");
         out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
         out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일자</td>");
-        /*
+
         for(int i=0;i<list.size();i++){
-            MemberVo membervo = (MemberVO) list.get(i);
-            String id = memberVO.getId();
-            String pwd = memberVO.getPwd();
-            String name = memberVO.getName();
-            String email = memberVO.getEmail();
-            Date joinDate = memberVO.getJoinDate();
+            MemberVO membervo = (MemberVO) list.get(i);
+            String id = membervo.getId();
+            String pwd = membervo.getPwd();
+            String name = membervo.getName();
+            String email = membervo.getEmail();
+            Date joinDate = membervo.getJoinDate();
             out.print("<tr><td>"+id+"</td><td>"+pwd+"</td><td>"+name+"</td><td>"+email+"</td><td>"+joinDate+"</td><td>");
         }
-        */
+
         out.print("</table></body></html>");
     }
 }
