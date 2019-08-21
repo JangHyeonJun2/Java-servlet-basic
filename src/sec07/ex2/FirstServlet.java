800124491417083ee7689cffe38b7e88c98d5e9c
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-08-21 21:58:57 +0900

    refresh를 이용한 포워딩 실습

---
+++ src/sec07/ex2/FirstServlet.java
@@ -0,0 +1,19 @@
+package sec07.ex2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first2")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        resp.addHeader("Refresh","1;url=second2");//웹 브라우저에 1초 후 SecondServlet.java로 재요청합니다.
+    }
+}
