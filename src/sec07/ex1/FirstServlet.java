ad0dae8ba2528c6ff17c67bf6370b8881c661731
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-08-21 21:46:56 +0900

    redirect를 이용한 포워딩 실습

--- src/sec07/ex1/FirstServlet.java
+++ src/sec07/ex1/FirstServlet.java
@@ -1,4 +1,19 @@
 package sec07.ex1;
 
-public class FirstServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first1")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        resp.sendRedirect("second1");
+    }
 }
--- src/sec07/ex1/SecondServlet.java
+++ src/sec07/ex1/SecondServlet.java
@@ -1,4 +1,21 @@
 package sec07.ex1;
 
-public class SecondServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/second1")
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("<html><body>");
+        out.print("sendRedirect를 이용한 redirect 실습입니다.");
+        out.print("</body></html>");
+    }
 }
