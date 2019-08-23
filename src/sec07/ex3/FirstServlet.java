eb18e8fe54088508da06e6d4d4fffd6de0e28d97
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-08-23 22:22:58 +0900

    location을 활용한 redirect실습하기

---
+++ src/sec07/ex3/FirstServlet.java
@@ -0,0 +1,21 @@
+package sec07.ex3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/first3")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("<script type = 'text/javascript'>");
+        out.print("location.href='second3'");
+        out.print("</script>");
+    }
+}
---
+++ src/sec07/ex3/SecondServlet.java
@@ -0,0 +1,21 @@
+package sec07.ex3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/second3")
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("<html><body>");
+        out.print("location을 이요한 redirect실습입니다.");
+        out.print("</body></html>");
+    }
+}
