3f5d935a48fe28e8652e8a6f509c43c181e54185
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-08-23 22:41:52 +0900

    서블릿을 이용한 dispatch 포워딩 실습 코드 작성

--- src/sec08/ex01/FirstServlet.java
+++ src/sec08/ex01/FirstServlet.java
@@ -1,4 +1,22 @@
 package sec08.ex01;
 
-public class FirstServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/first4")
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        /*
+         * dispatch 방법을 이용해 second로 전달합니다.
+         */
+        RequestDispatcher dispatcher = req.getRequestDispatcher("second4");
+        dispatcher.forward(req,resp);
+    }
 }
--- src/sec08/ex01/SecondServlet.java
+++ src/sec08/ex01/SecondServlet.java
@@ -1,4 +1,22 @@
 package sec08.ex01;
 
-public class SecondServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/second4")
+public class SecondServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print("<html><body>");
+        out.print("dispatch를 이용한 forward 실습입니다.");
+        out.print("</body></html>");
+    }
 }
