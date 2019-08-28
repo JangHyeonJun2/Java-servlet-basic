cf8620df2c95085c548464db5ea09184aa2d5dcc
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-08-28 18:50:37 +0900

    두 서블릿 간 회원정보 조회 바인딩 코드

--- src/sec10/ex01/MemberDAO.java
+++ src/sec10/ex01/MemberDAO.java
@@ -1,4 +1,110 @@
 package sec10.ex01;
 
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+/*
+ * DataSource를 이용한 DB접속
+ * 주석2
+ */
+
 public class MemberDAO {
+    private DataSource dataFactory;
+    public MemberDAO(){
+        Context ctx;
+
+        {
+            try {
+                ctx = new InitialContext();
+                Context envContext = (Context)ctx.lookup("java:/comp/env"); //이거 뭔지 다시 확인하기
+                dataFactory = (DataSource)envContext.lookup("jdbc/mysql");
+            } catch (NamingException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public List listmembers(){
+        List list = new ArrayList();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        String sql = "select * from t_member";
+
+        try{
+            conn = dataFactory.getConnection(); //데이터베이스 연결 메서드
+            pstmt = conn.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()){
+                String id = rs.getString("id");
+                String pwd = rs.getString("pwd");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                Date joinDate = rs.getDate("joinDate");
+
+                MemberVO vo = new MemberVO();
+                vo.setId(id);
+                vo.setPwd(pwd);
+                vo.setName(name);
+                vo.setEmail(email);
+                vo.setJoinDate(joinDate);
+                list.add(vo);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
+    /*
+    private Connection connDB(){
+        Connection conn = null;
+
+        String server = "localhost:3306"; // MySQL 서버 주소
+        String database = "study_db1"; // MySQL DATABASE 이름
+        String user_name = "root"; //  MySQL 서버 아이디
+        String password = "0653"; // MySQL 서버 비밀번호
+        //1.드라이버 로
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("mysql 드라이버 로딩 성공");
+        }catch (ClassNotFoundException e) {
+            System.err.println(" !! <JDBC 오류> Driver load 오류: " + e.getMessage());
+            e.printStackTrace();
+        }
+        //연결
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://" + server + "/" + database + "?allowPublicKeyRetrieval=true&useSSL=false&amp;autoReconnection=true", user_name, password);
+        } catch (SQLException e) {
+            System.err.println("con 오류:" + e.getMessage());
+            e.printStackTrace();
+        }
+        return conn;
+    }
+    */
+
 }
--- src/sec10/ex01/MemberServlet.java
+++ src/sec10/ex01/MemberServlet.java
@@ -1,4 +1,37 @@
 package sec10.ex01;
 
-public class MemberServlet {
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/member")
+public class MemberServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doHandle(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doHandle(req,resp);
+    }
+    private void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        MemberDAO dao = new MemberDAO();
+
+        List memberList = dao.listmembers();
+        req.setAttribute("membersList",memberList);
+        RequestDispatcher dispatch = req.getRequestDispatcher("viewMembers");
+        dispatch.forward(req,resp);
+
+
+    }
 }
