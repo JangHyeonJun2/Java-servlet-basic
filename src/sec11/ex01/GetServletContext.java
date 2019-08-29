7b0915249f9614e9fcc164a3d02cbbe59994d830
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-08-29 21:16:42 +0900

    ServletContext 바인딩 기능 코드 실습 완료

--- src/sec11/ex01/GetServletContext.java
+++ src/sec11/ex01/GetServletContext.java
@@ -1,4 +1,30 @@
 package sec11.ex01;
 
-public class GetServletContext {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/cget")
+public class GetServletContext extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        ServletContext context = getServletContext();//ServletContext객체를 가져온다.
+        List member = (ArrayList)context.getAttribute("member");
+        String name = (String)member.get(0);
+        int age = (Integer) member.get(1); //아마 자동 언박싱 되서 age변수에 들어갈듯??
+
+        out.print("<html><body>");
+        out.print("name:"+name+", age:"+age);
+        out.print("</body></html>");
+
+    }
 }
--- src/sec11/ex01/SetServletContext.java
+++ src/sec11/ex01/SetServletContext.java
@@ -1,4 +1,31 @@
 package sec11.ex01;
 
-public class SetServletContext {
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/cset")
+public class SetServletContext extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        ServletContext context = getServletContext(); // ServletContext 객체를 가져온다.
+        List member = new ArrayList();
+        member.add("이순신");
+        member.add(30);
+        context.setAttribute("member",member);//ServletContext 객체에 데이터를 바인딩 한다.
+        out.print("<html><body>");
+        out.print("이순신과 30 설정");
+        out.print("</body></html>");
+
+
+    }
 }
