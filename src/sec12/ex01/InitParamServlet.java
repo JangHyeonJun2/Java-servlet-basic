97ebbd79e62a27471e5a24ed14a56b73ea1864bf
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-09-04 21:52:43 +0900

    애너테이션을 이용한 서블릿 설정 실습에 주석 추가

--- src/sec12/ex01/InitParamServlet.java
+++ src/sec12/ex01/InitParamServlet.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "InitParamServlet",
-            urlPatterns = {"/sInit","/sInit2"},
+            urlPatterns = {"/sInit","/sInit2"},//urlPatterns를 이용해 매핑 이름을 여러 개 설정할 수 있습니다.
+
+             /* @WebinitParam을 이용해 여러 개의 매개변수를 설정할 수 있다.*/
             initParams = {@WebInitParam(name = "email",value = "admin@naver.com"),
                           @WebInitParam(name = "tel", value = "010-0000-1111")})
 public class InitParamServlet extends HttpServlet {
@@ -18,7 +20,7 @@ public class InitParamServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=utf-8");
         PrintWriter out = response.getWriter();
-        String email = getInitParameter("email");
+        String email = getInitParameter("email");//설정한 매개변수의 name으로 값을 가져온다.
         String tel = getInitParameter("tel");
 
         out.print("<html><body>");
